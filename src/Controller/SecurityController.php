d0ee01b720e1b468ddb72ab3ff5e16cb615d6536
TeddyAgt <teddy.aboim@gmail.com>
2024-08-25 18:58:00 +0200

    add signup

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/signup', name: 'signup')]
+    public function signup(EntityManagerInterface $em, Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $userForm = $this->createForm(UserType::class, $user);
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash("success", "Bienvenue sur Wonder !");
+            $this->redirectToRoute("login");
+        }
+
+        return $this->render('security/signup.html.twig', [
+            "form" => $userForm->createView()
+        ]);
+    }
+
+    #[Route('/login', name: 'login')]
+    public function login()
+    {
+        // 
+    }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout()
+    {
+        // 
+    }
+}
