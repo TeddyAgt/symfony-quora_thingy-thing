f47b9d2d877b43ceea4cffa3f45a4f5ef0922a7b
TeddyAgt <teddy.aboim@gmail.com>
2024-08-26 05:55:18 +0200

    add logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -38,7 +38,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils)
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute("home");
+            return $this->redirectToRoute("app_home");
         }
 
         $error = $authenticationUtils->getLastAuthenticationError();
